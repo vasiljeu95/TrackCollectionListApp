5097ecffc700dc501a02eff812abcdb36fc93199
vasiljeu95 <vasilyeu95@gmail.com>
2019-12-09 19:37:05 +0300

    Initial Commit

---
+++ TrackCollectionListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackCollectionListApp
+//
+//  Created by Stepan Vasilyeu on 12/9/19.
+//  Copyright © 2019 Stepan Vasilyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
